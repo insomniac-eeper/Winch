bcace04aeb53b07186562189e2bb72de9e2b03bd
insomniac-eeper <171281312+insomniac-eeper@users.noreply.github.com>
2024-09-09 03:00:49 +0200

    add BepInEx plugin project

---
+++ Winch.BepInExPlugin/Plugin.cs
@@ -0,0 +1,19 @@
+﻿namespace Winch.BepInExPlugin;
+
+using BepInEx;
+using BepInEx.Logging;
+using Core;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    public static ManualLogSource Log { get; private set; } = null;
+
+    private void Awake()
+    {
+        // Plugin startup logic
+        Log = Logger;
+        Log.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        WinchCore.Initialize();
+    }
+}
